909b969d14baf65435ba8fa7f8578bdc2c3ff567
SK.SAMEERUDDIN <shaik.18bcd7140@vitap.ac.in>
2020-04-08 16:23:40 +0530

    Create main.dart

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_phoenix/flutter_phoenix.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
+
+import 'src/app.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  BlocSupervisor.delegate = await HydratedBlocDelegate.build();
+  runApp(Phoenix(
+    child: App(),
+  ));
+}
