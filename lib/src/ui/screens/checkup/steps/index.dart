de85195b764fa9018081321ff7477e0ffda05e37
SK.SAMEERUDDIN <shaik.18bcd7140@vitap.ac.in>
2020-04-08 16:18:26 +0530

    Create index.dart

---
+++ lib/src/ui/screens/checkup/steps/index.dart
@@ -0,0 +1,14 @@
+  
+import 'package:flutter/material.dart';
+
+import 'intro.dart';
+import 'subjective.dart';
+import 'temperature.dart';
+
+abstract class CheckupStep extends Widget {}
+
+final List<CheckupStep> steps = [
+  IntroStep(),
+  SubjectiveStep(),
+  TemperatureStep(),
+];
