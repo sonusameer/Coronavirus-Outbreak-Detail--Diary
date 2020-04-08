4822e66f0e3d7f65cb57a43f69586a948f50215e
SK.SAMEERUDDIN <shaik.18bcd7140@vitap.ac.in>
2020-04-08 16:10:53 +0530

    Create questions_state.dart

---
+++ lib/src/blocs/questions/questions_state.dart
@@ -0,0 +1,35 @@
+import 'package:equatable/equatable.dart';
+
+import 'package:covidnearme/src/data/models/questions.dart';
+
+abstract class QuestionsState extends Equatable {
+  const QuestionsState();
+
+  @override
+  List<Object> get props => [];
+
+  @override
+  bool get stringify => true;
+}
+
+class QuestionsStateNotLoaded extends QuestionsState {}
+
+class QuestionsStateLoading extends QuestionsState {}
+
+class QuestionsStateLoaded extends QuestionsState {
+  final List<Question> questions;
+
+  const QuestionsStateLoaded(this.questions);
+
+  @override
+  List<Object> get props => [questions];
+}
+
+class QuestionsStateLoadingFailed extends QuestionsState {
+  final Exception exception;
+
+  const QuestionsStateLoadingFailed([this.exception]);
+
+  @override
+  List<Object> get props => [exception];
+}
