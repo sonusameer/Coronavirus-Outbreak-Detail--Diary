2d8a0cdeded326cce86e63ddbef7f42fe87517c6
SK.SAMEERUDDIN <shaik.18bcd7140@vitap.ac.in>
2020-04-08 16:05:45 +0530

    Create checkup_event.dart

---
+++ lib/src/blocs/checkup/checkup_event.dart
@@ -0,0 +1,26 @@
+import 'package:equatable/equatable.dart';
+
+import 'package:covidnearme/src/data/models/checkups.dart';
+
+abstract class CheckupEvent extends Equatable {
+  const CheckupEvent();
+
+  @override
+  List<Object> get props => [];
+
+  @override
+  bool get stringify => true;
+}
+
+class StartCheckup extends CheckupEvent {}
+
+class UpdateCheckup extends CheckupEvent {
+  final Checkup updatedCheckup;
+
+  const UpdateCheckup({this.updatedCheckup});
+
+  @override
+  List<Object> get props => [updatedCheckup];
+}
+
+class CompleteCheckup extends CheckupEvent {}
