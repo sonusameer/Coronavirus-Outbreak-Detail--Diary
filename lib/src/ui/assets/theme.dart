6848cb1dc552ce9c3571d89484acd38cf2b567c6
SK.SAMEERUDDIN <shaik.18bcd7140@vitap.ac.in>
2020-04-08 16:16:00 +0530

    Create theme.dart

---
+++ lib/src/ui/assets/theme.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+final ThemeData appTheme = ThemeData.light().copyWith(
+  appBarTheme: AppBarTheme(
+    color: Colors.lightBlue,
+  ),
+  dialogTheme: DialogTheme(
+    titleTextStyle: TextStyle(color: Colors.black),
+    contentTextStyle: TextStyle(color: Colors.black),
+  ),
+  scaffoldBackgroundColor: Colors.blue,
+  backgroundColor: Colors.white,
+  accentColor: Colors.yellowAccent,
+  inputDecorationTheme: InputDecorationTheme(
+    labelStyle: TextStyle(color: Colors.black),
+    border: OutlineInputBorder(
+      borderRadius: BorderRadius.circular(5),
+      borderSide: BorderSide.none,
+    ),
+    enabledBorder: OutlineInputBorder(
+      borderRadius: BorderRadius.circular(5),
+      borderSide: BorderSide.none,
+    ),
+    filled: true,
+    fillColor: Colors.white.withOpacity(0.8),
+    contentPadding: EdgeInsets.symmetric(vertical: 5, horizontal: 15),
+    errorStyle: TextStyle(
+      color: Colors.white,
+      fontSize: 16,
+    ),
+  ),
+  textTheme: ThemeData.dark().textTheme,
+  primaryTextTheme: ThemeData.dark().primaryTextTheme,
+  accentTextTheme: ThemeData.dark().accentTextTheme,
+  buttonTheme: ThemeData.dark().buttonTheme.copyWith(
+        padding: EdgeInsets.symmetric(vertical: 15, horizontal: 15),
+        buttonColor: Colors.white.withOpacity(0.8),
+        textTheme: ButtonTextTheme.primary,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10),
+          side: BorderSide.none,
+        ),
+      ),
+);
